0327fef7e7bcb2dcd566e7fca0eaf0600cc318e8
Mero0077 <omartalaatsaad2017@gmail.com>
2025-06-27 22:11:02 +0300

    AddReserve

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,10 +1,8 @@
 namespace Hotel_Management.Models
 {
-<<<<<<<<< Temporary merge branch 1
+
     public class Customer : BaseModel
-=========
-    public class Customer:BaseModel
->>>>>>>>> Temporary merge branch 2
+
     {
 
     }
